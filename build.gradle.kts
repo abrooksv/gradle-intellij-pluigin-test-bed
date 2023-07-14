2653c36a8254a0492a1d7a42d5de37d02f942c80
Austin Brooks <austin.brooks@datadoghq.com>
2023-07-14 08:01:20 -0700

    Setup example for Goland + tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,36 @@ version = properties("pluginVersion")
 // Configure project's dependencies
 repositories {
     mavenCentral()
+    maven("https://www.jetbrains.com/intellij-repository/releases")
+    maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
+}
+
+dependencies {
+    testImplementation("com.jetbrains.intellij.go:go-test-framework:231.9225.16") {
+        exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
+        exclude("org.jetbrains.kotlin", "kotlin-reflect")
+        exclude("com.jetbrains.rd", "rd-core")
+        exclude("com.jetbrains.rd", "rd-swing")
+        exclude("com.jetbrains.rd", "rd-framework")
+        exclude("org.jetbrains.teamcity", "serviceMessages")
+        exclude("io.ktor", "ktor-network-jvm")
+        exclude("com.jetbrains.infra", "download-pgp-verifier")
+        exclude("ai.grazie.utils", "utils-common-jvm")
+        exclude("ai.grazie.model", "model-common-jvm")
+        exclude("ai.grazie.model", "model-gec-jvm")
+        exclude("ai.grazie.model", "model-text-jvm")
+        exclude("ai.grazie.nlp", "nlp-common-jvm")
+        exclude("ai.grazie.nlp", "nlp-detect-jvm")
+        exclude("ai.grazie.nlp", "nlp-langs-jvm")
+        exclude("ai.grazie.nlp", "nlp-patterns-jvm")
+        exclude("ai.grazie.nlp", "nlp-phonetics-jvm")
+        exclude("ai.grazie.nlp", "nlp-similarity-jvm")
+        exclude("ai.grazie.nlp", "nlp-stemmer-jvm")
+        exclude("ai.grazie.nlp", "nlp-tokenizer-jvm")
+        exclude("ai.grazie.spell", "hunspell-en-jvm")
+        exclude("ai.grazie.spell", "gec-spell-engine-local-jvm")
+        exclude("ai.grazie.utils", "utils-lucene-lt-compatibility-jvm")
+    }
 }
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
